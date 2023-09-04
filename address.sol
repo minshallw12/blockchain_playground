f14e55f1f6de3eb070c21bd688f77e7aa6d3ce7f
minshallw12 <111906475+minshallw12@users.noreply.github.com>
2023-09-04 16:58:51 -0600

    address notes

---
+++ address.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.18;
+
+contract ExampleAddress {
+
+    address public someAddress;
+
+    function setSomeAddress(address _someAddress) public {
+        someAddress = _someAddress;
+    }
+
+    function getBalance() public view returns(uint) {
+        return someAddress.balance;
+    }
+
+}
